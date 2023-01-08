5b401111aaff72757cf809d98f8b1103ba2462ff
AoEiuV020 <aoeiuv020@gmail.com>
2023-01-08 20:28:34 +0800

    create project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HookMcMods
-hook MOD-MASTER for Minecraft PE
+xposed给'MOD-MASTER for Minecraft PE'去广告，  
+[![img](https://img.shields.io/badge/%E6%9B%B4%E6%96%B0%E6%97%A5%E5%BF%97-ChangeLog-brightgreen)](./ChangeLog.txt)
+[![CI](https://github.com/AoEiuV020/HookMcMods/actions/workflows/main.yml/badge.svg)](https://github.com/AoEiuV020/HookMcMods/actions/workflows/main.yml)
+[![img](https://img.shields.io/github/v/release/AoEiuV020/HookMcMods.svg)](https://github.com/AoEiuV020/HookMcMods/releases)
---
+++ hook/src/main/java/cc/aoeiuv020/hookmcmods/MainHook.java
@@ -0,0 +1,56 @@
+package cc.aoeiuv020.hookmcmods;
+
+import android.app.Application;
+import android.app.Instrumentation;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+@SuppressWarnings("RedundantThrows")
+public class MainHook implements IXposedHookLoadPackage {
+    @SuppressWarnings("All")
+    private static final boolean DEBUG = BuildConfig.DEBUG && false;
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("handleLoadPackage: " + lpparam.processName + ", " + lpparam.processName);
+        XposedHelpers.findAndHookMethod(Instrumentation.class, "callApplicationOnCreate", Application.class, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                if (!(param.args[0] instanceof Application)) return;
+                hookDebug(lpparam);
+                hookAdManager(lpparam);
+            }
+        });
+
+    }
+
+    private void log(XC_MethodHook.MethodHookParam param) {
+        XposedBridge.log("hook: " + param.thisObject.getClass().getName() + "." + param.method.getName());
+        if (DEBUG) {
+            XposedBridge.log(new Throwable());
+        }
+    }
+
+    private void hookDebug(XC_LoadPackage.LoadPackageParam lpparam) {
+        if (!DEBUG) {
+            return;
+        }
+        var r = new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                // 无功能，必要时断点使用的，
+                log(param);
+            }
+        };
+
+        XposedHelpers.findAndHookMethod(
+                "android.app.Activity",
+                lpparam.classLoader, "onResume", r
+        );
+    }
+
+}
